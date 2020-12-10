c9d981a9522542a380d7f6d615311a92d8ba0b71
Rupert Wu <wu.rupert@outlook.com>
2020-12-09 22:16:26 -0500

    Cleaned mse.py and neural_enhance results.

--- mse.py
+++ mse.py
@@ -1,17 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
-folder_name = "alexjc_neural-enhance/"
+A = plt.imread(sys.argv[1])
+B = plt.imread(sys.argv[2])
 
-# images = [
+if ".png" in sys.argv[1]:
+    A *= 256
+if ".png" in sys.argv[2]:
+    B *= 256
 
-# ]
-# png vs jpg mse needs to be scaled
+mse = ((A - B)**2).mean(axis=None)
 
-A =    plt.imread("alexjc_neural-enhance/salisbury-cathedral_ne2x_80.png")
-B = plt.imread("small-test-set_processed/salisbury-cathedral.jpg")
-A = A*256
-
-mse = ((A - B)**2).mean(axis=None)#/(A.shape[0]*A.shape[1])
-
-print (mse)
+print(mse)
