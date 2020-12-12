564d794e8b028b84c4d2162a4ef1977dae7b13d0
Rupert Wu <wu.rupert@outlook.com>
2020-12-12 17:45:31 -0500

    Using filenames arguments for mse.py.

--- mse.py
+++ mse.py
@@ -2,16 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-# A = plt.imread(sys.argv[1])
-# B = plt.imread(sys.argv[2])
+A = plt.imread(sys.argv[1])
+B = plt.imread(sys.argv[2])
 
-A = plt.imread("small-test-set_processed/salisbury-cathedral.jpg")
-B =                  plt.imread("opencv_/salisbury-cathedral.jpg")
-
-# if ".png" in sys.argv[1]:
-#     A *= 256
-# if ".png" in sys.argv[2]:
-#     B *= 256
+if ".png" in sys.argv[1]:
+    A *= 256
+if ".png" in sys.argv[2]:
+    B *= 256
 
 mse = ((A - B)**2).mean(axis=None)
 
